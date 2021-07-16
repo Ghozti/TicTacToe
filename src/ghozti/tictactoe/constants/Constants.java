9778d814360952b2266e518cd73c396f567365d1
Ghozti <ybalderas1005@gmail.com>
2021-07-16 18:03:45 -0400

    - added error Constants

--- src/ghozti/tictactoe/constants/Constants.java
+++ src/ghozti/tictactoe/constants/Constants.java
@@ -1,4 +1,15 @@
 package ghozti.tictactoe.constants;
 
 public class Constants {
+
+    public static class ErrorCodes extends Constants{
+
+        public static final String A_OK = "000";
+
+        public static final String INVALID_MOVE = "001";
+
+        public static final String PLAYER_WIN = "010";
+
+        public static final String AI_WIN = "011";
+    }
 }
--- src/ghozti/tictactoe/io/IOHandler.java
+++ src/ghozti/tictactoe/io/IOHandler.java
@@ -1,12 +1,14 @@
 package ghozti.tictactoe.io;
 
+import ghozti.tictactoe.constants.Constants;
 import ghozti.tictactoe.ui.Interface;
 
 public class IOHandler {
 
     public String verifyPlayerMove(int[] moves){
         if (!Interface.getGamePad()[moves[0]][moves[1]].equals(" ")){
-            return;
+            return Constants.ErrorCodes.A_OK;
         }
+        return Constants.ErrorCodes.INVALID_MOVE;
     }
 }
