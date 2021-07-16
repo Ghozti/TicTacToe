6d6d62cfe127ab40dc32ca30816e4fb4a92c2fdf
Ghozti <ybalderas1005@gmail.com>
2021-07-16 01:41:20 -0400

    - worked on Interface

--- src/ghozti/tictactoe/ui/Interface.java
+++ src/ghozti/tictactoe/ui/Interface.java
@@ -1,4 +1,28 @@
 package ghozti.tictactoe.ui;
 
 public class Interface {
+
+    private String[][] gamePad = {
+            {"","",""},
+            {"","",""},
+            {"","",""}
+    };
+
+    public void updateUI(int xMoveCoor, int yMoveCoor, String player){
+        if(!gamePad[xMoveCoor][yMoveCoor].equals(" ")){
+            gamePad[xMoveCoor][yMoveCoor] = player;
+        }else {
+            System.out.println("***PLACE ALREADY TAKEN***");
+        }
+    }
+
+    public void displayUI(){
+        for(String[] arr  : gamePad){
+            StringBuilder row = new StringBuilder();
+            for(String str : arr){
+                row.append(str);
+            }
+            System.out.println(row.toString());
+        }
+    }
 }
