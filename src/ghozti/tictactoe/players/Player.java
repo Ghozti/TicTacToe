122a9349a317d23365bc464c0958566bda1f7e99
Ghozti <ybalderas1005@gmail.com>
2021-07-25 01:42:48 -0400

    - worked on Player

--- src/ghozti/tictactoe/players/Player.java
+++ src/ghozti/tictactoe/players/Player.java
@@ -4,11 +4,21 @@ public class Player {
 
     private String move;
 
+    private static int[] lastPlayerMove;
+
     public Player(String move){
         this.move = move;
     }
 
     public String getMove(){
         return move;
+    }
+
+    public static int[] getLastPlayerMove(){
+        return lastPlayerMove;
+    }
+
+    public static void setLastPlayerMove(int[] coor){
+         coor = lastPlayerMove;
     }
 }
